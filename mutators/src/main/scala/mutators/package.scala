f6fd4d9d35593a7a2aa345d8f023f9dcce7e16ff
Ricardo Martins <rmartins@growin.pt>
2019-07-24 16:10:04 +0100

    rename

--- mutators/src/main/scala/mutators/package.scala
+++ mutators/src/main/scala/mutators/package.scala
@@ -2,7 +2,7 @@ import scala.meta.Term
 
 package object mutators {
 
-  implicit class TermToMutationTerm(terms: Seq[Term]) {
+  implicit class TermsToMutatedTerm(terms: Seq[Term]) {
     def toMutation(needsParens: Boolean): MutatedTerms = MutatedTerms(terms, needsParens)
   }
 
