7055b64d22596c7f94110e2b65448f12e8eb3ea1
Ricardo Martins <ricardocmartins91@gmail.com>
2019-08-23 10:22:29 +0100

    Some refactoring to make the code more readable and maintainable

---
+++ mutators/src/main/scala/mutators/Mutant.scala
@@ -0,0 +1,24 @@
+package mutators
+
+import play.api.libs.json.{Json, OWrites}
+
+import scala.meta.Term
+
+case class Mutant(
+    id: Int,
+    diffLines: List[String],
+    original: Term,
+    mutated: Term,
+    mutationType: String = ""
+)
+
+object Mutant {
+  implicit val jsonWrites: OWrites[Mutant] =
+    (mutant: Mutant) =>
+      Json.obj(
+        "id" -> mutant.id,
+        "diff" -> mutant.diffLines,
+        "original" -> mutant.original.syntax,
+        "mutated" -> mutant.mutated.syntax
+      )
+}
