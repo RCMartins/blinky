72939e2ab1ad672e148127f2c4288e3ee8cbdb3a
Ricardo Martins <ricardocmartins91@gmail.com>
2019-11-17 14:38:21 +0000

    fixed Cli

--- blinky-cli/src/main/scala/blinky/cli/Cli.scala
+++ blinky-cli/src/main/scala/blinky/cli/Cli.scala
@@ -1,9 +1,13 @@
 package blinky.cli
 
 import blinky.run.Run
+import os.Path
 
 object Cli {
   def main(args: Array[String]): Unit = {
-    Run.run()
+    args match {
+      case confPath +: _ => Run.run(Path(confPath))
+      case _             => Run.run()
+    }
   }
 }
