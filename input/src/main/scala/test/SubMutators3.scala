d1926f1bda6077c6d720176f3a612082b56a1c63
Ricardo Martins <ricardocmartins91@gmail.com>
2019-12-30 16:28:18 +0000

    Added one more test to improve mutation score (#39)

---
+++ input/src/main/scala/test/SubMutators3.scala
@@ -0,0 +1,13 @@
+/*
+rule = Blinky
+Blinky.enabledMutators = [
+  { ConditionalExpressions = [AndToOr, OrToAnd, RemoveUnaryNot] }
+  { LiteralStrings = [EmptyToMutated, NonEmptyToMutated, ConcatToMutated] }
+  { ScalaOptions = [GetOrElse, Exists, Forall, IsEmpty, NonEmpty, Fold,
+                    OrElse, OrNull, Filter, FilterNot, Contains] }
+  { ScalaTry = [GetOrElse, OrElse] }
+]
+ */
+package test
+
+object SubMutators3 {}
