80262c74dfcb634ea04ff8765836a43e8f6e39eb
Ricardo Martins <ricardocmartins91@gmail.com>
2019-12-30 18:20:06 +0000

    Fixed bug & some renaming (#40)

--- output/src/main/scala/test/LiteralStrings.scala
+++ output/src/main/scala/test/LiteralStrings.scala
@@ -4,4 +4,5 @@ object LiteralStrings {
   val string1 = if (???) "mutated!" else ""
   val string2 = if (???) "" else if (???) "mutated!" else "a cool string"
   val string3 = if (???) "mutated!" else if (???) "" else string2 + ", or is it?"
+  val string4 = if (???) "" else "mutated!"
 }
