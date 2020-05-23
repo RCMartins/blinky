108987eb5a444f5b59f0a9166f582681d51cd11a
Ricardo Martins <ricardocmartins91@gmail.com>
2020-05-23 11:23:56 +0100

    ...

--- travis/setup.sh
+++ travis/setup.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-export PATH="$PATH:$(pwd)/bin"
-type amm >/dev/null 2>&1 || sudo sh -c '(echo "#!/usr/bin/env sh" && curl -L https://github.com/lihaoyi/Ammonite/releases/download/2.1.4/2.12-2.1.4) > /usr/local/bin/amm && chmod +x /usr/local/bin/amm'
-export PATH="$PATH:$HOME/.local/share/coursier/bin"
-coursier install bloop --only-prebuilt=true
+export PATH="$PATH:$(pwd)/bin:$HOME/.local/share/coursier/bin"
+coursier setup --apps ammonite,bloop --yes
