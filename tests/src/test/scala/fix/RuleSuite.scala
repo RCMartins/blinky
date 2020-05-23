a96223206df037fcd152a2ff13305224c579b287
Ricardo Martins <ricardocmartins91@gmail.com>
2020-05-23 17:28:51 +0100

    ...

--- tests/src/test/scala/fix/RuleSuite.scala
+++ tests/src/test/scala/fix/RuleSuite.scala
@@ -133,7 +133,7 @@ class RuleSuite extends SemanticRuleSuite() {
       test(testData.ruleTest.path.testName + " (mutants file)") {
         val mutantsFile: File = mutantsFileResolver(testData.ruleTest.path.testPath)
 
-        if (mutantsFile.isSameContentAs(mutantsExpectedFile))
+        if (mutantsFile.isSimilarContentAs(mutantsExpectedFile))
           succeed
         else {
           println(s"""Actual:
