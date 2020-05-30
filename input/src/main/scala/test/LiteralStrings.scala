6e426f3ec30b298a03a226f597a0ef649af6973a
Ricardo Martins <ricardocmartins91@gmail.com>
2020-05-30 11:45:08 +0100

    fixed tests

--- input/src/main/scala/test/LiteralStrings.scala
+++ input/src/main/scala/test/LiteralStrings.scala
@@ -12,4 +12,8 @@ object LiteralStrings {
   val string4 = "mutated!"
   val string5 = s""
   val string6 = s"$string2-test"
+  val string7 = f""
+  val string8 = f"$string2%20s-test"
+  val string9 = raw""
+  val string10 = raw"$string2\test"
 }
--- output/src/main/scala/test/LiteralStrings.scala
+++ output/src/main/scala/test/LiteralStrings.scala
@@ -7,8 +7,8 @@ object LiteralStrings {
   val string4 = if (???) "" else "mutated!"
   val string5 = if (???) "mutated!" else s""
   val string6 = if (???) "" else if (???) "mutated!" else s"$string2-test"
-  val string5 = if (???) "mutated!" else s""
-  val string6 = if (???) "" else if (???) "mutated!" else s"$string2-test"
-  val string5 = if (???) "mutated!" else s""
-  val string6 = if (???) "" else if (???) "mutated!" else s"$string2-test"
+  val string7 = if (???) "mutated!" else f""
+  val string8 = if (???) "" else if (???) "mutated!" else f"$string2%20s-test"
+  val string9 = if (???) "mutated!" else raw""
+  val string10 = if (???) "" else if (???) "mutated!" else raw"$string2\test"
 }
