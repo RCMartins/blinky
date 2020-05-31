d26cfba263af4563e0b60cf047baab71f0b62faa
Ricardo Martins <ricardocmartins91@gmail.com>
2020-05-31 14:50:20 +0100

    Added tests to ConsoleReporter

--- blinky-cli/src/test/scala/blinky/cli/TestSpec.scala
+++
@@ -1,7 +0,0 @@
-package blinky.cli
-
-import org.scalatest.{AppendedClues, OptionValues}
-import org.scalatest.matchers.must.Matchers
-import org.scalatest.wordspec.AnyWordSpec
-
-trait TestSpec extends AnyWordSpec with Matchers with OptionValues with AppendedClues
