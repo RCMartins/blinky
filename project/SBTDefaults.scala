7f63ea3d8d8a6d5e2fa2c6248724d2ee522d06ca
Ricardo Martins <ricardocmartins91@gmail.com>
2020-06-06 11:25:01 +0100

    sbt stuff

---
+++ project/SBTDefaults.scala
@@ -0,0 +1,45 @@
+object SBTDefaults {
+
+  val defaultScalacFlags: List[String] =
+    List(
+      "-encoding",
+      "UTF-8",
+      "-explaintypes",
+      "-unchecked",
+      "-feature",
+      "-deprecation:true",
+      "-Xfuture",
+      "-Xcheckinit",
+      "-Xlint:by-name-right-associative",
+      "-Xlint:constant",
+      "-Xlint:delayedinit-select",
+      "-Xlint:doc-detached",
+      "-Xlint:missing-interpolator",
+      "-Xlint:option-implicit",
+      "-Xlint:package-object-classes",
+      "-Xlint:poly-implicit-overload",
+      "-Xlint:private-shadow",
+      "-Xlint:stars-align",
+      "-Xlint:type-parameter-shadow",
+      "-Xlint:unsound-match",
+      "-Ywarn-dead-code",
+      "-Ywarn-inaccessible",
+      "-Ywarn-nullary-override",
+      "-Ywarn-nullary-unit",
+      "-Ywarn-numeric-widen",
+      "-Ywarn-extra-implicit",
+      "-Ywarn-infer-any",
+      "-Ywarn-unused:imports",
+      "-Ywarn-unused:locals",
+      "-Ywarn-unused:patvars",
+      "-Ywarn-unused:privates",
+      "-Ypartial-unification",
+      "-Yno-adapted-args",
+      "-Ywarn-unused:implicits",
+      "-Ywarn-unused:params",
+      "-Ywarn-macros:after",
+      "-Yrangepos",
+      "-Xfatal-warnings"
+    )
+
+}
