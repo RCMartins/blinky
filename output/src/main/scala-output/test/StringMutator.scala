882e89a79053d8da5bbc56c0061b3651d3aec192
Ricardo Martins <ricardocmartins91@gmail.com>
2020-07-06 19:22:50 +0100

    fixed tests

--- output/src/main/scala-output/test/StringMutator.scala
+++ output/src/main/scala-output/test/StringMutator.scala
@@ -5,7 +5,7 @@ import java.util.Locale
 object StringMutator {
   val string1 = if (???) " Foo " else " Foo ".trim
 
-  val string2 = if (???) "mutated!" else if (???) "" else string1 + ", or is it?"
+  val string2 = if (???) "mutated!" else if (???) "" else string1 + ", or is it?" + " who knows?"
   val string2b = if (???) "mutated!" else if (???) "" else string1.concat(", or is it?")
 
   val string3 = if (???) string1 else string1.toUpperCase
