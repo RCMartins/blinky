6389f8d0b4dc16c2c1dedf22d89e9dcc3962050a
Ricardo Martins <ricardocmartins91@gmail.com>
2021-02-15 23:06:39 +0000

    Update setup.sh

--- travis/setup.sh
+++ travis/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Temporary fix the weird bug with downloading this library...
-mkdir -p ~/.cache/coursier/v1/https/repo1.maven.org/maven2/org/bouncycastle/1.64
-curl https://repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk15on/1.64/bcprov-jdk15on-1.64.pom > ~/.cache/coursier/v1/https/repo1.maven.org/maven2/org/bouncycastle/1.64/bcprov-jdk15on-1.64.pom
-curl https://repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk15on/1.64/bcprov-jdk15on-1.64.jar > ~/.cache/coursier/v1/https/repo1.maven.org/maven2/org/bouncycastle/1.64/bcprov-jdk15on-1.64.jar
+mkdir -p ~/.cache/coursier/v1/https/repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk/1.64
+curl https://repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk15on/1.64/bcprov-jdk15on-1.64.pom > ~/.cache/coursier/v1/https/repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk/1.64/bcprov-jdk15on-1.64.pom
+curl https://repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk15on/1.64/bcprov-jdk15on-1.64.jar > ~/.cache/coursier/v1/https/repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk/1.64/bcprov-jdk15on-1.64.jar
 # ----------
 
 type cs >/dev/null 2>&1 ||
