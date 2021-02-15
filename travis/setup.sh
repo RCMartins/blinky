b9147248fc7932611e19394387923d3774056a59
Ricardo Martins <ricardocmartins91@gmail.com>
2021-02-15 22:48:22 +0000

    Update setup.sh

--- travis/setup.sh
+++ travis/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Temporary fix the weird bug with downloading this library...
+curl https://repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk15on/1.64/bcprov-jdk15on-1.64.pom > ~/.cache/coursier/v1/https/repo1.maven.org/maven2/org/bouncycastle/1.64/bcprov-jdk15on-1.64.pom
+curl https://repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk15on/1.64/bcprov-jdk15on-1.64.jar > ~/.cache/coursier/v1/https/repo1.maven.org/maven2/org/bouncycastle/1.64/bcprov-jdk15on-1.64.jar
+# ----------
+
 type cs >/dev/null 2>&1 ||
   (mkdir -p bin &&
     curl -fLo bin/cs https://git.io/coursier-cli-linux &&
