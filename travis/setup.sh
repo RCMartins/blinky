121300ba37b3ff2f95bfd5d853fdce21fc50f919
Ricardo Martins <ricardocmartins91@gmail.com>
2021-02-16 10:38:36 +0000

    ...

--- travis/setup.sh
+++ travis/setup.sh
@@ -6,8 +6,8 @@ curl https://repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk15on/1.64/bcprov-
 curl https://repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk15on/1.64/bcprov-jdk15on-1.64.jar > ~/.cache/coursier/v1/https/repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk15on/1.64/bcprov-jdk15on-1.64.jar
 # ----------
 
-type cs >/dev/null 2>&1 ||
-  (mkdir -p bin &&
-    curl -fLo bin/cs https://git.io/coursier-cli-linux &&
-    chmod +x bin/cs) &&
-  cs setup --apps ammonite,bloop -y
+#type cs >/dev/null 2>&1 ||
+#  (mkdir -p bin &&
+#    curl -fLo bin/cs https://git.io/coursier-cli-linux &&
+#    chmod +x bin/cs) &&
+#  cs setup --apps ammonite,bloop -y
