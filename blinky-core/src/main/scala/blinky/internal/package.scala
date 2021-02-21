87cf89350b34097009a280a2ee75cce85bd56084
Ricardo Martins <ricardocmartins91@gmail.com>
2021-02-21 13:00:55 +0000

    Fixed diff issue with missing parens

--- blinky-core/src/main/scala/blinky/internal/package.scala
+++ blinky-core/src/main/scala/blinky/internal/package.scala
@@ -3,7 +3,12 @@ package blinky
 import scala.meta.Term
 
 package object internal {
+
   implicit class TermsToMutatedTerm(terms: Seq[Term]) {
     def toMutated(needsParens: Boolean): MutatedTerms = MutatedTerms(terms, needsParens)
   }
+
+  def syntaxParens(term: Term, needsParens: Boolean): String =
+    if (needsParens) "(" + term.syntax + ")" else term.syntax
+
 }
