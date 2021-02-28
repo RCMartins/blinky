a7956d870cd02635228a2996d4d9e6b491a16af8
Ricardo Martins <ricardocmartins91@gmail.com>
2021-02-28 15:53:09 +0000

    Update sbt-scalafix to 0.9.24

--- output/src/main/scala-output/test/PartialFunctionsMutators2.scala
+++ output/src/main/scala-output/test/PartialFunctionsMutators2.scala
@@ -3,7 +3,8 @@ package test
 object PartialFunctionsMutators2 {
   case class Foo(str: String, bool: Boolean)
 
-  ('a' to 'z').toList.collect (if (???) {
+  ('a' to 'z').toList.collect {
+    (if (???) {
   case c @ 'a' => c
   case 'i' | 'o' => '#'
 } else if (???) {
@@ -19,12 +20,14 @@ object PartialFunctionsMutators2 {
   case c @ ('a' | 'e') => c
   case 'i' | 'o' => '#'
 })
+  }
 
-  List(Foo("bar", true)).collect (if (???) {
+  List(Foo("bar", true)).collect { (if (???) {
   case Foo("abc", _) => 123
 } else if (???) {
   case Foo("bar", _) => 123
 } else {
   case Foo("abc" | "bar", _) => 123
 })
+  }
 }
