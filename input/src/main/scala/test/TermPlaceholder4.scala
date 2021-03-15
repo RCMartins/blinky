e3549ec67f55d8b50f121db2c140ddef9b45fcd3
Ricardo Martins <ricardocmartins91@gmail.com>
2021-03-15 21:00:00 +0000

    improved tests

--- input/src/main/scala/test/TermPlaceholder4.scala
+++ input/src/main/scala/test/TermPlaceholder4.scala
@@ -8,10 +8,15 @@ package test
 
 object TermPlaceholder4 {
 
-  def concat3(str1: String, str2: String): String = str1 + "test" + str2
+  def concat2(str1: String, str2: String): String = str1 + "test" + str2
 
-  val func1: (String, String) => Int = concat3(_, _).length + 5
+  def concat3(str1: String, str2: String, str3: String): String =
+    str1 + "test" + str2 + "foo" + str3
 
-  val func2: (String, String) => String = concat3(_, _) + (1 + 1).toString
+  val func1: (String, String) => Int = concat2(_, _).length + 5
+
+  val func2: (String, String) => String = concat2(_, _) + (1 + 1).toString
+
+  val func3: (String, String, String) => Int = concat3(_, _, _).length + 5
 
 }
--- output/src/main/scala-output/test/TermPlaceholder4.scala
+++ output/src/main/scala-output/test/TermPlaceholder4.scala
@@ -2,10 +2,16 @@ package test
 
 object TermPlaceholder4 {
 
-  def concat3(str1: String, str2: String): String = str1 + "test" + str2
+  def concat2(str1: String, str2: String): String = str1 + "test" + str2
 
-  val func1: (String, String) => Int = (_1_, _2_) => if (???) concat3(_1_, _2_).length - 5 else concat3(_1_, _2_).length + 5
+  def concat3(str1: String, str2: String, str3: String): String =
+    str1 + "test" + str2 + "foo" + str3
 
-  val func2: (String, String) => String = concat3(_, _) + (if (???) 1 - 1 else 1 + 1).toString
+  val func1: (String, String) => Int = (_1_, _2_) => if (???) concat2(_1_, _2_).length - 5 else concat2(_1_, _2_).length + 5
+
+  val func2: (String, String) => String = concat2(_, _) + (if (???) 1 - 1 else 1 + 1).toString
+
+  val func3: (String, String, String) => Int = (_3_, _4_, _5_) => ///
+    if (???) concat3(_3_, _4_, _5_).length - 5 else concat3(_3_, _4_, _5_).length + 5
 
 }
