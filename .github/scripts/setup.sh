75937deaa536db5201817860967c8568eeee80e5
Ricardo Martins <ricardocmartins91@gmail.com>
2021-03-16 21:51:19 +0000

    Fixed release script

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -5,4 +5,4 @@ mkdir -p ~/.cache/coursier/v1/https/repo1.maven.org/maven2/org/bouncycastle/bcpr
 curl https://repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk15on/1.64/bcprov-jdk15on-1.64.pom > ~/.cache/coursier/v1/https/repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk15on/1.64/bcprov-jdk15on-1.64.pom
 curl https://repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk15on/1.64/bcprov-jdk15on-1.64.jar > ~/.cache/coursier/v1/https/repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk15on/1.64/bcprov-jdk15on-1.64.jar
 
-git fetch --tags && git fetch --unshallow && git branch master origin/master
+git fetch --tags && git branch master origin/master
