ce5a3d8f9d896832dc60486fdd1801af341263ca
Ricardo Martins <ricardocmartins91@gmail.com>
2021-03-21 15:56:44 +0000

    improved tests

--- output/src/main/scala-output/test/SpecificMutantIndices.scala
+++ output/src/main/scala-output/test/SpecificMutantIndices.scala
@@ -9,4 +9,5 @@ object SpecificMutantIndices {
   val string7 = if (_root_.scala.sys.env.contains("BLINKY_MUTATION_8")) "" else s"$string2"
   val string8 = s"test"
   val string9 = if (_root_.scala.sys.env.contains("BLINKY_MUTATION_12")) "mutated!" else f""
+  val string10 = Some(string2).map(_1_ => if (_root_.scala.sys.env.contains("BLINKY_MUTATION_14")) "" else _1_ + "!!")
 }
