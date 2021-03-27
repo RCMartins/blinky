197ee85dc2c33ee3f8a46e3d1c37db3fceb4d160
Ricardo Martins <ricardocmartins91@gmail.com>
2021-03-27 19:34:38 +0000

    Added new ci job to test community scala projects

---
+++ project/RunCommunityProjects.scala
@@ -0,0 +1,55 @@
+import ammonite.ops._
+import os.copy
+
+object RunCommunityProjects {
+
+  private case class Project(url: String, folderName: String, blinkyConf: String)
+
+  private def defaultBlinkyConf(projectName: String): String =
+    s"""projectPath = "."
+       |projectName = "$projectName"
+       |filesToMutate = "core/src/main"
+       |""".stripMargin
+
+  private val pluginsText = """addSbtPlugin("ch.epfl.scala" % "sbt-bloop" % "1.4.8")"""
+
+  private val projects: Map[String, Project] =
+    Map(
+      "spire" -> Project(
+        "https://github.com/typelevel/spire.git",
+        "spire",
+        defaultBlinkyConf("testsJVM")
+      )
+    )
+
+  def run(versionNumber: String, projectsToRun: Array[String]): Unit =
+    projectsToRun.foreach { projectToRun =>
+      projects.get(projectToRun) match {
+        case None =>
+          Console.err.println(s"Project not found: $projectToRun")
+        case Some(project) =>
+          println(s"Testing $projectToRun project:")
+          testProject(versionNumber, project)
+      }
+    }
+
+  private def testProject(versionNumber: String, project: Project): Unit = {
+    val tempPath: Path = tmp.dir()
+    %("git", "clone", project.url)(tempPath)
+    val projectPath = tempPath / project.folderName
+
+    write(projectPath / ".blinky.conf", project.blinkyConf)
+
+    write.append(projectPath / "project" / "plugins.sbt", pluginsText)
+
+    %(
+      "cs",
+      "launch",
+      s"com.github.rcmartins:blinky-cli_2.12:$versionNumber",
+      "--",
+      "--verbose=true",
+      "--dryRun=true"
+    )(projectPath)
+  }
+
+}
