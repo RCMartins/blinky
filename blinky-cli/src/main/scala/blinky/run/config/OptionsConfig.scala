0383fff33afe6df6c83b096f7dbece156865c66c
Ricardo Martins <ricardocmartins91@gmail.com>
2022-06-26 14:34:42 +0100

    added more tests

--- blinky-cli/src/main/scala/blinky/run/config/OptionsConfig.scala
+++ blinky-cli/src/main/scala/blinky/run/config/OptionsConfig.scala
@@ -41,31 +41,45 @@ object OptionsConfig {
   )
 
   implicit val durationDecoder: ConfDecoder[Duration] =
-    ConfDecoder.fromPartial[Duration]("String") { case Conf.Str(durationStr) =>
-      Configured.Ok(Duration(durationStr))
+    ConfDecoder.fromPartial[Duration]("Duration string") {
+      case Conf.Str(durationStr) if Try(Duration(durationStr)).isSuccess =>
+        Configured.Ok(Duration(durationStr))
     }
 
-  implicit val doubleDecoder: ConfDecoder[Double] = ConfDecoder.fromPartial[Double]("number") {
-    case Conf.Num(number) => Configured.Ok(number.toDouble)
-  }
+  implicit val doubleDecoder: ConfDecoder[Double] =
+    ConfDecoder.fromPartial[Double]("Number") { case Conf.Num(number) =>
+      Configured.Ok(number.toDouble)
+    }
+
+  private val InvalidMultiRunConfIndex: String =
+    "Invalid index value, should be >= 1"
+  private val InvalidMultiRunConfAmount: String =
+    "Invalid amount, should be bigger than index"
+  private val InvalidMultiRunConfFormat: String =
+    "Invalid value, should be a String in 'int/int' format"
+  private val InvalidMultiRunConfFormatShort: String =
+    "String in 'int/int' format"
 
   def stringToMultiRunParser: String => Either[String, (Int, Int)] =
     (str: String) =>
       Try(str.split("/").toList.map(_.toInt)) match {
-        case Success(List(index, amount)) if index >= 1 && amount >= index =>
+        case Success(List(index, _)) if index < 1 =>
+          Left(InvalidMultiRunConfIndex)
+        case Success(List(index, amount)) if index > amount =>
+          Left(InvalidMultiRunConfAmount)
+        case Success(List(index, amount)) =>
           Right((index, amount))
-        case Success(_) =>
-          Left("Invalid values, they should be >= 1")
-        case Failure(_) =>
-          Left("Invalid value, should be in 'int/int' format")
+        case Success(_) | Failure(_) =>
+          Left(InvalidMultiRunConfFormat)
       }
 
   implicit val multiRunDecoder: ConfDecoder[(Int, Int)] =
-    ConfDecoder.fromPartial[(Int, Int)]("String") { case Conf.Str(multiRunStr) =>
-      stringToMultiRunParser(multiRunStr) match {
-        case Right(multiRunValue) => Configured.Ok(multiRunValue)
-        case Left(message)        => Configured.error(message)
-      }
+    ConfDecoder.fromPartial[(Int, Int)](InvalidMultiRunConfFormatShort) {
+      case conf @ Conf.Str(multiRunStr) =>
+        stringToMultiRunParser(multiRunStr) match {
+          case Right(multiRunValue) => Configured.Ok(multiRunValue)
+          case Left(message)        => Configured.typeMismatch(message, conf)
+        }
     }
 
   implicit val surface: Surface[OptionsConfig] =
---
+++ blinky-cli/src/test/scala/blinky/run/config/OptionsConfigTest.scala
@@ -0,0 +1,125 @@
+package blinky.run.config
+
+import blinky.TestSpec
+import metaconfig.{Conf, Configured}
+import zio.test.Assertion._
+import zio.test._
+import zio.test.environment.TestEnvironment
+
+object OptionsConfigTest extends TestSpec {
+
+  val spec: Spec[TestEnvironment, TestFailure[Nothing], TestSuccess] =
+    suite("OptionsConfig")(
+      test("Return error when duration config is not a string") {
+        assert(OptionsConfig.durationDecoder.read(Conf.fromInt(50)))(
+          equalTo(
+            Configured.error(
+              """Type mismatch;
+                |  found    : Number (value: 50)
+                |  expected : Duration string""".stripMargin
+            )
+          )
+        )
+      },
+      test("Return error when duration config is an invalid duration") {
+        assert(OptionsConfig.durationDecoder.read(Conf.fromString("50 potatoes")))(
+          equalTo(
+            Configured.error(
+              """Type mismatch;
+                |  found    : String (value: "50 potatoes")
+                |  expected : Duration string""".stripMargin
+            )
+          )
+        )
+      },
+      test("Return error when double config is not a number") {
+        assert(OptionsConfig.doubleDecoder.read(Conf.fromBoolean(false)))(
+          equalTo(
+            Configured.error(
+              """Type mismatch;
+                |  found    : Boolean (value: false)
+                |  expected : Number""".stripMargin
+            )
+          )
+        )
+      },
+      test("Return error when multiRun config is not a string") {
+        assert(OptionsConfig.multiRunDecoder.read(Conf.fromInt(100)))(
+          equalTo(
+            Configured.error(
+              """Type mismatch;
+                |  found    : Number (value: 100)
+                |  expected : String in 'int/int' format""".stripMargin
+            )
+          )
+        )
+      },
+      test("Return error when multiRun config is invalid (index < 1)") {
+        assert(OptionsConfig.multiRunDecoder.read(Conf.fromString("0/2")))(
+          equalTo(
+            Configured.error(
+              """Type mismatch;
+                |  found    : String (value: "0/2")
+                |  expected : Invalid index value, should be >= 1""".stripMargin
+            )
+          )
+        )
+      },
+      test("Return error when multiRun config is invalid (index > amount)") {
+        assert(OptionsConfig.multiRunDecoder.read(Conf.fromString("2/1")))(
+          equalTo(
+            Configured.error(
+              """Type mismatch;
+                |  found    : String (value: "2/1")
+                |  expected : Invalid amount, should be bigger than index""".stripMargin
+            )
+          )
+        )
+      },
+      test("Return error when multiRun config is invalid #1") {
+        assert(OptionsConfig.multiRunDecoder.read(Conf.fromString("1-2")))(
+          equalTo(
+            Configured.error(
+              """Type mismatch;
+                |  found    : String (value: "1-2")
+                |  expected : Invalid value, should be a String in 'int/int' format""".stripMargin
+            )
+          )
+        )
+      },
+      test("Return error when multiRun config is invalid #2") {
+        assert(OptionsConfig.multiRunDecoder.read(Conf.fromString("1/all")))(
+          equalTo(
+            Configured.error(
+              """Type mismatch;
+                |  found    : String (value: "1/all")
+                |  expected : Invalid value, should be a String in 'int/int' format""".stripMargin
+            )
+          )
+        )
+      },
+      test("Return error when multiRun config is invalid #3") {
+        assert(OptionsConfig.multiRunDecoder.read(Conf.fromString("1/all/2")))(
+          equalTo(
+            Configured.error(
+              """Type mismatch;
+                |  found    : String (value: "1/all/2")
+                |  expected : Invalid value, should be a String in 'int/int' format""".stripMargin
+            )
+          )
+        )
+      },
+      test("Return error when multiRun config is invalid #4") {
+        assert(OptionsConfig.multiRunDecoder.read(Conf.fromString("1")))(
+          equalTo(
+            Configured.error(
+              """Type mismatch;
+                |  found    : String (value: "1")
+                |  expected : Invalid value, should be a String in 'int/int' format""".stripMargin
+            )
+          )
+        )
+      }
+    )
+
+}
