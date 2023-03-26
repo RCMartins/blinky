d8d8825d86a66c14de8d1ebd41ce1446fa71c52d
Ricardo Martins <ricardocmartins91@gmail.com>
2023-03-26 23:26:40 +0100

    ...

--- output/src/main/scala-output/test/GeneralForYield.scala
+++ output/src/main/scala-output/test/GeneralForYield.scala
@@ -4,7 +4,7 @@ object GeneralForYield {
 
   val list: IndexedSeq[String] =
     for {
-      char <- 'a' to (if (???) 'x' - 1 else 'x' + 1).toChar
+      char <- 'a' to ((if (???) 'x' - 1 else 'x' + 1)).toChar
       if char != 'q'
       between = if (???) "" else if (???) "mutated!" else s"'$char'"
       case n: Int <- 1 to (if (???) 1 - 1 else 1 + 1)
