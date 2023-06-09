c0ac73f4bbc59b43c8bdb36fd9b75314ddc0aebc
RCMartins <ricardocmartins91@gmail.com>
2023-06-10 00:16:00 +0100

    Fixed bug with RemoveOneCase mutator

--- output/src/main/scala-output/test/PartialFunctionsMutators1.scala
+++ output/src/main/scala-output/test/PartialFunctionsMutators1.scala
@@ -3,21 +3,25 @@ package test
 object PartialFunctionsMutators1 {
   val value1 =
     List(10, 20, 30).map (if (???) {
+  case 10 if false => true
   case 20 => false
   case 30 => true
   case _ => false
 } else if (???) {
   case 10 => true
+  case 20 if false => false
   case 30 => true
   case _ => false
 } else if (???) {
   case 10 => true
   case 20 => false
+  case 30 if false => true
   case _ => false
 } else if (???) {
   case 10 => true
   case 20 => false
   case 30 => true
+  case _ if false => false
 } else {
   case 10 => true
   case 20 => false
@@ -32,10 +36,15 @@ object PartialFunctionsMutators1 {
 
   val value3 =
     List(10, 20).map (if (???) {
+  case 10 if false =>
+    5
   case _ =>
     1 + 1
 } else if (???) {
-  case 10 => 5
+  case 10 =>
+    5
+  case _ if false =>
+    1 + 1
 } else if (???) {
   case 10 =>
     5
