cafc668a32b1328aa6a07c61fcd2bacc403b0395
Ricardo Martins <ricardocmartins91@gmail.com>
2023-06-17 23:37:24 +0100

    Added 'scalameta' library community test

--- project/RunCommunityProjects.scala
+++ project/RunCommunityProjects.scala
@@ -2,23 +2,25 @@ import os.{Path, ProcessOutput}
 
 object RunCommunityProjects {
 
-  private case class Project(url: String, folderName: String, blinkyConf: String)
+  private val Bloop = "bloop"
+  private val SBT = "sbt"
 
-  private def defaultBlinkyConf(projectName: String): String =
-    s"""projectPath = "."
-       |projectName = "$projectName"
-       |filesToMutate = "core/src/main"
-       |""".stripMargin
-
-  private val pluginsText = """addSbtPlugin("ch.epfl.scala" % "sbt-bloop" % "1.4.13")"""
+  private val sbtBloopVersion = "1.5.6"
+  private val pluginsText = s"""addSbtPlugin("ch.epfl.scala" % "sbt-bloop" % "$sbtBloopVersion")"""
 
   private val projects: Map[String, Project] =
     Map(
-      "spire" -> Project(
-        "https://github.com/typelevel/spire.git",
-        "spire",
-        defaultBlinkyConf("testsJVM")
-      )
+      "scalameta" ->
+        Project(
+          url = "https://github.com/scalameta/scalameta.git",
+          folderName = "scalameta",
+          projectName = "",
+          filesToMutate = "scalameta",
+          compileCommand = "testsJVM/compile",
+          testCommand = "testsJVM/test",
+          hash = Some("ccd351c4bb681e1bdf44e3a9f0c789e0158be7f3"),
+          testRunner = SBT,
+        )
     )
 
   def run(versionNumber: String, projectsToRun: Array[String]): Unit =
@@ -36,23 +38,45 @@ object RunCommunityProjects {
     val tempPath: Path = os.temp.dir()
     os.proc("git", "clone", project.url).call(cwd = tempPath)
     val projectPath = tempPath / project.folderName
+    project.hash.foreach(hash => os.proc("git", "checkout", hash).call(cwd = projectPath))
+    project.extraCommands(projectPath)
 
-    os.write(projectPath / ".blinky.conf", project.blinkyConf)
+    os.write(projectPath / ".blinky.conf", blinkyConf(project))
 
-    os.write.append(projectPath / "project" / "plugins.sbt", pluginsText)
+    if (project.testRunner == Bloop)
+      os.write.append(projectPath / "project" / "plugins.sbt", pluginsText)
 
     os.proc(
       "cs",
       "launch",
       s"com.github.rcmartins:blinky-cli_2.13:$versionNumber",
-      "--",
-      "--verbose=true",
-      "--dryRun=true"
     ).call(
       cwd = projectPath,
       stdout = ProcessOutput.Readlines(println),
       stderr = ProcessOutput.Readlines(Console.err.println)
     )
   }
+
+  private final case class Project(
+      url: String,
+      folderName: String,
+      projectName: String,
+      filesToMutate: String,
+      compileCommand: String,
+      testCommand: String,
+      hash: Option[String],
+      extraCommands: Path => Unit = _ => (),
+      testRunner: String = SBT,
+  )
+
+  private def blinkyConf(project: Project): String =
+    s"""projectPath = "."
+       |projectName = "${project.projectName}"
+       |filesToMutate = "${project.filesToMutate}"
+       |options.testRunner = "${project.testRunner}"
+       |options.compileCommand = "${project.compileCommand}"
+       |options.testCommand = "${project.testCommand}"
+       |options.dryRun = true
+       |""".stripMargin
 
 }
