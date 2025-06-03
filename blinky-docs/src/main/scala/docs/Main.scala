038c61ef561c2ef4095a03f7909b8247b4cca6bb
Ricardo Martins <ricardocmartins91@gmail.com>
2025-06-03 21:06:46 +0100

    Updated documentation (#658)

--- blinky-docs/src/main/scala/docs/Main.scala
+++ blinky-docs/src/main/scala/docs/Main.scala
@@ -1,12 +1,19 @@
 package docs
 
-import java.nio.file.Paths
+import better.files.File
 
+import java.nio.file.Paths
 import blinky.BuildInfo
 
 object Main {
 
   def main(args: Array[String]): Unit = {
+    val ExtractVersion = """.*scalaVersion := "(.*)".*""".r
+    val scala212Version: String =
+      File("ci-tests/examples/default/build.sbt").lines
+        .collectFirst { case ExtractVersion(version) => version }
+        .getOrElse("Failed to extract Scala 2.12 version from ci-tests/examples/default/build.sbt")
+
     val settings =
       mdoc
         .MainSettings()
@@ -15,7 +22,8 @@ object Main {
           Map(
             "STABLE_VERSION" -> BuildInfo.stable,
             "SNAPSHOT_VERSION" -> BuildInfo.version,
-            "SCALA_VERSION" -> BuildInfo.scalaVersion,
+            "SCALA_VERSION_212" -> scala212Version,
+            "SCALA_VERSION_213" -> BuildInfo.scalaVersion,
             "SBT_VERSION" -> BuildInfo.sbtVersion
           )
         )
